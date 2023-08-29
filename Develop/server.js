829d3bcc32b04e1e448f20296d8ee38a319221c5
Fancy Nancy <ramireznancy.686@gmail.com>
2023-08-28 22:12:53 -0600

    added get api function that reads the db.json

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const app = express();
 const path = require("path");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
-const app = express();
+
 const PORT = 3001;
 const notesData = require("./db/db.json");
 console.log(notesData);
@@ -11,15 +12,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/*", (req, res) =>
+app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+app.get("/api/notes", (req, res) => {
+  const notesData = fs.readFileSync("./db/db.json", "utf8");
+  const notes = JSON.parse(notesData);
+  res.json(notes);
+});
+
 app.post("/api/notes", (req, res) => {
-  const notesData = fs.readFileSync("db/db.json", "utf8");
+  const notesData = fs.readFileSync("./db/db.json", "utf8");
   const notes = JSON.parse(notesData);
 
   //new note with a unique ID
